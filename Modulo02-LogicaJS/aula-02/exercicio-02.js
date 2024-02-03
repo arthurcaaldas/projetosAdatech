9520961d4127f08e4c13a3a1f8644a03c05f9365
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-03 16:12:24 -0300

    aula02 logica

---
+++ Modulo02-LogicaJS/aula-02/exercicio-02.js
@@ -0,0 +1,31 @@
+/* 
+    Crie um jogo de adivinhação em que o programa gera um número aleatório entre 
+    1 e 100 e o jogador tenta adivinhar o número. O jogo deve dar dicas ao jogador, 
+    informando se o número é maior ou menor do que a tentativa atual. O jogador 
+    deve continuar a adivinhar até acertar o número. No final, deverá informar a 
+    quantidade de tentativas.
+*/
+
+const numeroSorteado = Math.floor(Math.random() * 100 + 1);
+let numeroEscolhido = parseInt(prompt("Digite um número"));
+let chute = 0;
+
+while (numeroEscolhido !== numeroSorteado) {
+  switch (true) {
+    case numeroEscolhido < numeroSorteado:
+      console.log("Número abaixo");
+      break;
+
+    case numeroEscolhido > numeroSorteado:
+      console.log("Número acima");
+      break;
+
+    default:
+      console.log("Opção inválida");
+  }
+
+  numeroEscolhido = parseInt(prompt("Tente novamente. Digite outro número: "));
+  chute++;
+}
+
+console.log(`Parabéns! Você acertou em ${chute + 1} tentativas.`);
--- Modulo02-LogicaJS/aula-02/script.js
+++ Modulo02-LogicaJS/aula-02/script.js
@@ -22,3 +22,42 @@
 // and &&
 // or ||
 // not !!
+
+// operador ternario
+// const diaDeSol = true;
+// const tenhoDinheiro = false;
+
+// tenhoDinheiro && diaDeSol ? console.log("vou a praia") : console.log("vou ficar em casa")
+
+
+// let today = new Date().getDay();
+
+// switch (today) {
+//   case 0:
+//     console.log("Hoje é Domingo");
+//     break;
+  
+//   case 1:
+//     console.log("hoje é Segunda-Feira");
+//     break;
+  
+//   case 2:
+//     console.log("hoje é Terça-Feira");
+//     break;
+  
+//   case 3:
+//     console.log("hoje é Quarta-Feira");
+//     break;
+  
+//   case 4:
+//     console.log("hoje é Quinta-Feira");
+//     break;
+  
+//   case 5:
+//     console.log("hoje é Sexta-Feira");
+//     break;
+  
+//   case 6:
+//     console.log("hoje é Sábado");
+//     break;
+// }

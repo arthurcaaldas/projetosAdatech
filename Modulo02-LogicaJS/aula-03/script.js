ecb86db89d3f54df3d3b5b2ef6380c6879848b60
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-05 20:00:44 -0300

    aula03 - estrutura de repeticao for tabuada

--- Modulo02-LogicaJS/aula-03/script.js
+++ Modulo02-LogicaJS/aula-03/script.js
@@ -1,4 +1,9 @@
-for (let i = 0; i < array.length; i++) {
-  console.log(i);
-  
+// for (let i = 0; i < array.length; i++) {
+//   console.log(i);
+// }
+
+for (let i = 0; i <= 10; i++) {
+  let tabuada = i + '*' + 10 + '=' + i * 10;
+
+  console.log(tabuada);
 }

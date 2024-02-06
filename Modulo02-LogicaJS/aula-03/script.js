b6228df2e9fd43aa0c5f3fe0a1781811cf79df56
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-05 21:28:51 -0300

    aula03 - estrutura de repeticao for of

--- Modulo02-LogicaJS/aula-03/script.js
+++ Modulo02-LogicaJS/aula-03/script.js
@@ -10,4 +10,37 @@
 //   console.log(tabuada);
 // }
 
-// FOR IN
+// FOR IN - usado para objetos
+
+// const alunos = [
+//   {nome: "Joao", idade: 12},
+//   {nome: "Carlos", idade: 24},
+//   {nome: "Andre", idade: 21},
+//   {nome: "Diogo", idade: 25},
+//   {nome: "Pedro", idade: 30},
+//   {nome: "Williams", idade: 17},
+// ]
+
+// for (const i in alunos) {
+//   console.log(`${alunos[i].nome} tem ${alunos[i].idade}`);
+    
+// }
+
+// FOR OF - usado para pecorrer objetos, array, map...
+
+const alunos = [
+  {nome: "Joao", idade: 12},
+  {nome: "Carlos", idade: 24},
+  {nome: "Andre", idade: 21},
+  {nome: "Diogo", idade: 25},
+  {nome: "Pedro", idade: 30},
+  {nome: "Williams", idade: 17},
+]
+
+for (const aluno of alunos) {
+  console.log(`${aluno.nome} tem ${aluno.idade} anos`)
+}
+
+for (const {nome, idade}of alunos) {
+  console.log(`${nome} tem ${idade} anos`)
+}

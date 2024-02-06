b67e2052e5dc771bb19fb606755959451e827281
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-05 21:47:17 -0300

    aula03 - ex02

---
+++ Modulo02-LogicaJS/aula-03/ex02.js
@@ -0,0 +1,24 @@
+/*
+Você é um professor, e tem uma lista com alguns alunos (sendo objetos), 
+que contém nome, nota1 e nota2. Use o for para percorer esse array e 
+calcular a média das duas notas de cada aluno. Ao final, imprimir o 
+nome do aluno e a sua média
+*/
+const listaDeAlunos = [
+  { nome: "João", notas: [8, 7] },
+  { nome: "Maria", notas: [9, 8] },
+  { nome: "Carlos", notas: [6, 5] },
+  { nome: "Ana", notas: [7, 6] },
+  { nome: "Pedro", notas: [10, 9] },
+  { nome: "Lúcia", notas: [8, 7] },
+  { nome: "Fernando", notas: [7, 6] },
+  { nome: "Camila", notas: [9, 8] },
+  { nome: "Rafael", notas: [6, 5] },
+  { nome: "Juliana", notas: [10, 9] },
+];
+
+for (let i = 0; i < listaDeAlunos.length; i++) {
+  const aluno = listaDeAlunos[i];
+  const media = (aluno.notas[0] + aluno.notas[1]) / (aluno.notas.length);
+  console.log(`${aluno.nome} teve a média de ${media}`);
+}

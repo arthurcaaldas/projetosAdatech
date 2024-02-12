acb805dd28c2a2b427d82fc0f45f392f22370d97
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-12 10:12:44 -0300

    montando corpo do email

--- Modulo02-LogicaJS/projetoIndividual/week.js
+++ Modulo02-LogicaJS/projetoIndividual/week.js
@@ -3,3 +3,10 @@ function verificarDiaSemanaAtual() {
   const diaSemana = hoje.getDay();
   return diaSemana === 1;
 }
+
+function montarCorpoEmail() {
+  return `
+  <h1>CarStore</h1>
+  <p></p>
+  `;
+}

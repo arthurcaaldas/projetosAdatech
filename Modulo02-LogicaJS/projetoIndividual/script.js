ff6be214be3e3716f6d12ef70f6f605f1ffe15d0
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-15 10:01:41 -0300

    acrescentando subtarefas

--- Modulo02-LogicaJS/projetoIndividual/script.js
+++ Modulo02-LogicaJS/projetoIndividual/script.js
@@ -6,35 +6,35 @@ const enviarEmail = (addressee, subject, body) => {
       message: "Um destinatário precisa ser fornecido ao enviar um e-mail.",
     };
   }
-
+  
   if (!subject) {
     return {
       status: "Error",
       message:
-        "O campo de assunto não deveria estar vazio ao enviar um e-mail.",
+      "O campo de assunto não deveria estar vazio ao enviar um e-mail.",
     };
   }
-
+  
   if (!body) {
     return {
       status: "Error",
       message: "O corpo da mensagem precisa ser fornecido ao enviar um e-mail.",
     };
   }
-
+  
   console.log(
     `
-        De: news@carstore.com
-        Para: ${addressee}
-        Assunto: ${subject}
-        
-        ${body}
-        
-        CarStore - Aqui você encontra o seu carro novo
-      `
-  );
-
-  return { status: "Sucess", message: "E-mail enviado com sucesso!" };
-};
-
-module.exports = enviarEmail;
+    De: news@carstore.com
+    Para: ${addressee}
+    Assunto: ${subject}
+    
+    ${body}
+    
+    CarStore - Aqui você encontra o seu carro novo
+    `
+    );
+    
+    return { status: "Sucess", message: "E-mail enviado com sucesso!" };
+  };
+  
+  module.exports = enviarEmail;
--- Modulo02-LogicaJS/projetoIndividual/week.js
+++ Modulo02-LogicaJS/projetoIndividual/week.js
@@ -8,43 +8,88 @@ const cadastro = [
   { nome: "cliente07", email: "client07@example.com", flag: true }
 ];
 
-function DiaSemana() {
-  const hoje = new Date();
-  const diaSemana = hoje.getDay();
-  return diaSemana === 4;
-}
+let cadastroValidado = [];
 
-function enviarEmail() {
-  const diaSemana = DiaSemana();
-  
-  if (diaSemana) {
-    const novos = ["320i, 2022", "Audi A4, 2020", "Jetta gti, 2021"];
-    const maisVendidos = ["Onix Ltz, 2019", "Ford Ka, 2020", "HB20, 2018"];
-    
-    const aquisicao = `
-    Não perca a oportunidade única de adquirir um dos nossos veículos!
-    Oferecemos financiamento com taxa de juros zero e primeira parcela para daqui a três meses.
-    Além disso, estamos dando um desconto especial para os 10 primeiros clientes que comprarem um dos carros mais vendidos.
-    Venha conferir em nossa concessionária CarStore!
-    `;
-    
-    for (const cliente of cadastro) {
-      if (cliente.flag === true) {
-        console.log("Enviando e-mail para:", cliente.email);
-        console.log("Olá! Você acaba de receber um Email de novidades da nossa concessionária CarStore.");
-        console.log("Novos veículos:", novos);
-        console.log("Mais vendidos:", maisVendidos);
-        console.log("Condições de aquisição:", aquisicao);
-      }
+function validarFlag() {
+  for (let i = 0; i < cadastro.length; i++) {
+    if (cadastro[i].flag === true) {   
+      cadastroValidado.push(cadastro[i]);
     }
-  } else {
-    console.log("Hoje não é segunda-feira. Nenhum email será enviado.");
+  }    
+}  
+validarFlag();
+
+const enviarEmail = (addressee, subject, body) => {
+  if (!addressee) {
+    return {
+      status: "Error",
+      message: "Um destinatário precisa ser fornecido ao enviar um e-mail.",
+    };
   }
-}
-
-enviarEmail();
-
-
-
-
-
+  
+  if (!subject) {
+    return {
+      status: "Error",
+      message: "O campo de assunto não deveria estar vazio ao enviar um e-mail.",
+    };
+  }
+  
+  if (!body) {
+    return {
+      status: "Error",
+      message: "O corpo da mensagem precisa ser fornecido ao enviar um e-mail.",
+    };
+  }
+  
+  console.log(
+    `
+    De: news@carstore.com
+    Para: ${addressee}
+    Assunto: ${subject}
+    
+    ${body}
+    
+    CarStore - Aqui você encontra o seu carro novo
+    `
+    );
+    
+    return { status: "Sucess", message: "E-mail enviado com sucesso!" };
+  };
+  
+  module.exports = enviarEmail;
+  
+  function DiaSemana() {
+    const hoje = new Date();
+    const diaSemana = hoje.getDay();
+    return diaSemana === 4;
+  }
+  
+  function enviarEmailSegunda() {
+    const diaSemana = DiaSemana();
+    
+    if (diaSemana) {
+      const novos = ["320i, 2022", "Audi A4, 2020", "Jetta gti, 2021"];
+      const maisVendidos = ["Onix Ltz, 2019", "Ford Ka, 2020", "HB20, 2018"];
+      
+      const aquisicao = `
+      Não perca a oportunidade única de adquirir um dos nossos veículos!
+      Oferecemos financiamento com taxa de juros zero e primeira parcela para daqui a três meses.
+      Além disso, estamos dando um desconto especial para os 10 primeiros clientes que comprarem um dos carros mais vendidos.
+      Venha conferir em nossa concessionária CarStore!
+      `;
+      
+      for (const cliente of cadastroValidado) {
+        const emailStatus = enviarEmail(cliente.email, "Novidades na CarStore", `
+        Olá ${cliente.nome}! Você acaba de receber um Email de novidades da nossa concessionária CarStore.
+        Novos veículos: ${novos.join(", ")}
+        Mais vendidos: ${maisVendidos.join(", ")}
+        Condições de aquisição: ${aquisicao}
+        `);
+        console.log(emailStatus);
+      }
+    } else {
+      console.log("Hoje não é segunda-feira. Nenhum email será enviado.");
+    }
+  }
+  
+  enviarEmailSegunda();

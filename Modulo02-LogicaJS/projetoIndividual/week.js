9b1023735bb5aa85437f1889a6d4198672021c3c
arthurcaaldas <arthurcaaldas@hotmail.com>
2024-02-15 09:30:28 -0300

    enviar email apenas para flag true

--- Modulo02-LogicaJS/projetoIndividual/week.js
+++ Modulo02-LogicaJS/projetoIndividual/week.js
@@ -1,31 +1,23 @@
-const cadastro = (nome, email, flag) => {
-  nome: "cliente01", email: "client01@example.com", flag: true,
-  nome: "cliente02", email: "client02@example.com", flag: true,
-  nome: "cliente03", email: "client03@example.com", flag: false,
-  nome: "cliente04", email: "client04@example.com", flag: true,
-  nome: "cliente05", email: "client05@example.com", flag: false,
-  nome: "cliente06", email: "client06@example.com", flag: false,
-  nome: "cliente07", email: "client07@example.com", flag: true
-};
-
-for (let i = 0; i < cadastro.length; i++) {
-  console.log("E-mail " + (i+1) + ": " + emails[i]);
-}
-
-
-
-
+const cadastro = [
+  { nome: "cliente01", email: "client01@example.com", flag: true },
+  { nome: "cliente02", email: "client02@example.com", flag: true },
+  { nome: "cliente03", email: "client03@example.com", flag: false },
+  { nome: "cliente04", email: "client04@example.com", flag: true },
+  { nome: "cliente05", email: "client05@example.com", flag: false },
+  { nome: "cliente06", email: "client06@example.com", flag: false },
+  { nome: "cliente07", email: "client07@example.com", flag: true }
+];
 
 function DiaSemana() {
   const hoje = new Date();
   const diaSemana = hoje.getDay();
-  return diaSemana === 1;
+  return diaSemana === 4;
 }
 
-function enviarEmailSegunda() {
+function enviarEmail() {
   const diaSemana = DiaSemana();
   
-  if (diaSemana === true) {
+  if (diaSemana) {
     const novos = ["320i, 2022", "Audi A4, 2020", "Jetta gti, 2021"];
     const maisVendidos = ["Onix Ltz, 2019", "Ford Ka, 2020", "HB20, 2018"];
     
@@ -36,17 +28,21 @@ function enviarEmailSegunda() {
     Venha conferir em nossa concessionária CarStore!
     `;
     
-    console.log("Olá! Você acaba de receber um Email de novidades da nossa concessionária CarStore.");
-    console.log("Novos veículos:", novos);
-    console.log("Mais vendidos:", maisVendidos);
-    console.log("Condições de aquisição:", aquisicao);
+    for (const cliente of cadastro) {
+      if (cliente.flag === true) {
+        console.log("Enviando e-mail para:", cliente.email);
+        console.log("Olá! Você acaba de receber um Email de novidades da nossa concessionária CarStore.");
+        console.log("Novos veículos:", novos);
+        console.log("Mais vendidos:", maisVendidos);
+        console.log("Condições de aquisição:", aquisicao);
+      }
+    }
   } else {
     console.log("Hoje não é segunda-feira. Nenhum email será enviado.");
   }
 }
 
-console.log(enviarEmailSegunda());
-
+enviarEmail();
 
 
 
